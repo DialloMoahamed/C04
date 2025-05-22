f823c0350928361505a6acef3d0c3fe89c25de0c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-05-22 15:43:49 +0100

    Commit

---
+++ sort_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int n, c;
+    int tableau[n];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &tableau[i]);
+    }
+    
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            if (tableau[i] > tableau[j])
+            {
+                c = tableau[i];
+                tableau[i] = tableau[j];
+                tableau[j] = c;
+            }
+            
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", tableau[i]);
+    }
+    printf("\n");
+    
+    
+    return 0;
+}
